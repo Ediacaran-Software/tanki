f4a09936f7d0f5797e87da497502b5090e55cab8
YilunSun <osirisnero@gmail.com>
2020-12-17 00:13:24 -0500

    first unstable version =D

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,256 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="top-nav-bar">
+    <div class="header">
+      <span class="title">Tanki</span>
+      <svg
+        t="1608169142456"
+        class="icon"
+        viewBox="0 0 1055 1024"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        p-id="1098"
+        width="54"
+        height="54"
+      >
+        <path
+          d="M287.34318 609.277884l33.948908 44.418839-7.836774 67.913489 407.512283 2.617483-20.892842-36.582064 20.892842-73.132781zM269.052148 392.465676l80.98523-10.454258v-86.204521h-75.765938zM582.523135 327.153996h232.485758v49.63813H582.523135z"
+          fill="#ffffff"
+          p-id="1099"
+        ></path>
+        <path
+          d="M822.861341 627.58459H232.485758a15.673549 15.673549 0 0 1-15.67355-15.67355v-114.934137a15.673549 15.673549 0 0 1 15.67355-15.673549H728.820045a15.673549 15.673549 0 0 1 12.131327 5.752192l94.041296 114.934138a15.689223 15.689223 0 0 1-12.131327 25.594906z m-574.702034-31.347099H789.790152l-68.399369-83.587039H248.159307v83.587039z"
+          fill="#ffffff"
+          p-id="1100"
+        ></path>
+        <path
+          d="M723.58508 742.550074H305.634212a88.916045 88.916045 0 0 1-88.822004-88.822004v-43.102261a15.673549 15.673549 0 0 1 31.347099 0v43.102261a57.553273 57.553273 0 0 0 57.474905 57.474906h417.950868a57.5376 57.5376 0 0 0 57.474905-57.474906v-37.882969a15.673549 15.673549 0 0 1 31.347099 0v37.882969a88.916045 88.916045 0 0 1-88.822004 88.822004z"
+          fill="#ffffff"
+          p-id="1101"
+        ></path>
+        <path
+          d="M284.725698 732.095817c-33.133883 0-60.076715-26.958505-60.076715-60.076715s26.942831-60.076715 60.076715-60.076715 60.076715 26.958505 60.076714 60.076715-26.942831 60.076715-60.076714 60.076715z m0-88.822004c-15.845958 0-28.729616 12.883658-28.729616 28.729616s12.883658 28.729616 28.729616 28.729616c15.845958 0 28.729616-12.883658 28.729616-28.729616s-12.883658-28.729616-28.729616-28.729616zM744.477921 732.095817c-33.133883 0-60.076715-26.958505-60.076715-60.076715s26.958505-60.076715 60.076715-60.076715 60.076715 26.958505 60.076714 60.076715-26.942831 60.076715-60.076714 60.076715z m0-88.822004c-15.845958 0-28.729616 12.883658-28.729616 28.729616s12.883658 28.729616 28.729616 28.729616 28.729616-12.883658 28.729616-28.729616-12.867984-28.729616-28.729616-28.729616zM602.365849 738.631687a15.673549 15.673549 0 0 1-15.67355-15.673549v-23.510324a15.673549 15.673549 0 0 1 31.347099 0v23.510324a15.673549 15.673549 0 0 1-15.673549 15.673549zM546.473972 738.631687a15.673549 15.673549 0 0 1-15.67355-15.673549v-23.510324a15.673549 15.673549 0 0 1 31.347099 0v23.510324a15.673549 15.673549 0 0 1-15.673549 15.673549zM490.566421 738.631687a15.673549 15.673549 0 0 1-15.673549-15.673549v-23.510324a15.673549 15.673549 0 0 1 31.347099 0v23.510324a15.673549 15.673549 0 0 1-15.67355 15.673549zM434.674544 738.631687a15.673549 15.673549 0 0 1-15.673549-15.673549v-23.510324a15.673549 15.673549 0 0 1 31.347099 0v23.510324a15.673549 15.673549 0 0 1-15.67355 15.673549zM378.766994 738.631687a15.673549 15.673549 0 0 1-15.67355-15.673549v-23.510324a15.673549 15.673549 0 0 1 31.347099 0v23.510324a15.673549 15.673549 0 0 1-15.673549 15.673549zM658.273399 738.631687a15.673549 15.673549 0 0 1-15.673549-15.673549v-23.510324a15.673549 15.673549 0 0 1 31.347099 0v23.510324a15.673549 15.673549 0 0 1-15.67355 15.673549zM767.988245 568.824453h-274.287114a15.673549 15.673549 0 0 1 0-31.347099h274.287114a15.673549 15.673549 0 0 1 0 31.347099zM577.303843 506.130256a15.673549 15.673549 0 0 1-15.673549-15.67355V312.797024h-18.291032a15.673549 15.673549 0 0 1 0-31.347098h33.964581a15.673549 15.673549 0 0 1 15.67355 15.673549v193.333231a15.673549 15.673549 0 0 1-15.67355 15.67355zM478.027582 312.797024h-15.67355a15.673549 15.673549 0 0 1 0-31.347098h15.67355a15.673549 15.673549 0 0 1 0 31.347098zM274.27144 508.747739a15.673549 15.673549 0 0 1-15.673549-15.67355V297.123475a15.673549 15.673549 0 0 1 15.673549-15.673549h125.388395a15.673549 15.673549 0 0 1 0 31.347098h-109.714845v180.277165a15.673549 15.673549 0 0 1-15.67355 15.67355z"
+          fill="#ffffff"
+          p-id="1102"
+        ></path>
+        <path
+          d="M822.845668 393.782254H637.380558a15.673549 15.673549 0 0 1 0-31.347099h169.79156v-20.892841h-227.266465a15.673549 15.673549 0 0 1 0-31.347099h242.940015a15.673549 15.673549 0 0 1 15.673549 15.67355v52.23994a15.673549 15.673549 0 0 1-15.673549 15.673549zM352.639187 396.399737h-78.367747a15.673549 15.673549 0 0 1 0-31.347099h62.694198v-65.31168a15.673549 15.673549 0 0 1 31.347098 0v80.985229a15.673549 15.673549 0 0 1-15.673549 15.67355z"
+          fill="#ffffff"
+          p-id="1103"
+        ></path>
+      </svg>
+      <span class="sm-text">for World of Tanks</span>
+    </div>
+
+    <Search
+      background="#555555"
+      shape="round"
+      class="search-bar"
+      v-model="value"
+      placeholder="Search by tank name"
+      @search="onSearch"
+      clear-trigger="none"
+    />
   </div>
-  <router-view/>
+  <div id="nav">
+    <div style="padding: 20px; display:flex; justify-content: space-around">
+      <Button class="tier-btn" type="success" @click="() => getTankDataByTier(1)">I</Button>
+      <Button class="tier-btn" type="success" @click="() => getTankDataByTier(2)">II</Button>
+      <Button class="tier-btn" type="success" @click="() => getTankDataByTier(3)">III</Button>
+      <Button class="tier-btn" type="success" @click="() => getTankDataByTier(4)">IV</Button>
+      <Button class="tier-btn" type="success" @click="() => getTankDataByTier(5)">V</Button>
+      <Button class="tier-btn" type="success" @click="() => getTankDataByTier(6)">VI</Button>
+      <Button class="tier-btn" type="success" @click="() => getTankDataByTier(7)">VII</Button>
+      <Button class="tier-btn" type="success" @click="() => getTankDataByTier(8)">VIII</Button>
+      <Button class="tier-btn" type="success" @click="() => getTankDataByTier(9)">IX</Button>
+      <Button class="tier-btn" type="success" @click="() => getTankDataByTier(10)">X</Button>
+    </div>
+  </div>
+  <div>
+    <ul class="tank-data-container">
+      <li class="tank-data" v-for="tankData in tankDataDisplay" :key="tankData.tank_id">
+        <h3>{{ tankData.name }}</h3>
+        <img class="img" v-bind:src="tankData.images.big_icon" draggable="false" />
+      </li>
+    </ul>
+  </div>
 </template>
 
+<script>
+import axios from "axios";
+import { ref, onMounted } from "vue";
+import Button from "vant/lib/button";
+import "vant/lib/button/style";
+import Toast from "vant/lib/toast";
+import "vant/lib/toast/style";
+import Search from "vant/lib/search";
+import "vant/lib/search/style";
+
+export default {
+  setup() {
+    const value = ref("");
+
+    const tankDataDisplay = ref([]);
+    const allTankData = [];
+    let hasAllTankData = false;
+
+    onMounted(() => {
+      Toast.loading({
+        duration: 0,
+        message: "Fetching data from server...",
+        // forbidClick: true,
+      });
+    });
+
+    function getTankDataByTier(tier) {
+      const url = `https://api.worldoftanks.com/wot/encyclopedia/vehicles/?application_id=9ac9f425f534dca03b4ab84d40bb7310&tier=${tier}`;
+
+      if (hasAllTankData) {
+        tankDataDisplay.value = allTankData.filter((tankData) => tankData.tier === tier);
+        return;
+      }
+      axios.get(url).then(
+        (response) => {
+          const tempTankDatas = [];
+          Object.values(response.data.data).forEach((tankData) => {
+            if (tankData.tier === tier) {
+              tempTankDatas.push(tankData);
+            }
+          });
+          tankDataDisplay.value = tempTankDatas;
+        },
+        (error) => (this.content = error)
+      );
+    }
+
+    const getMatchScore = (str1, str2) => {
+      // * DP
+      const dp = new Array(str1.length);
+      for (let i = 0; i < str1.length; i++) {
+        dp[i] = new Array(str2.length).fill(0);
+      }
+
+      // first row
+      for (let i = 0; i < str2.length; i++) {
+        dp[0][i] = str2.charAt(i) === str1.charAt(0) ? 1 : 0;
+      }
+
+      // first column
+      for (let i = 0; i < str1.length; i++) {
+        dp[i][0] = str1.charAt(i) === str2.charAt(0) ? 1 : 0;
+      }
+
+      // other dp
+      for (let i = 1; i < str1.length; i++) {
+        for (let j = 1; j < str2.length; j++) {
+          // if (str1.charAt(i) === str2.charAt(j)) {
+          //   dp[i][j] = Math.max(dp[i - 1][j - 1] + 1, dp[i - 1][j], dp[i][j - 1]);
+          // } else {
+          //   dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]);
+          // }
+          dp[i][j] = Math.max(
+            dp[i - 1][j - 1] + (str1.charAt(i) === str2.charAt(j) ? 1 : 0),
+            dp[i - 1][j],
+            dp[i][j - 1]
+          );
+        }
+      }
+
+      return dp[str1.length - 1][str2.length - 1];
+    };
+
+    function getTankDataByName(name) {
+      name = name.trim();
+
+      if (!hasAllTankData) {
+        Toast.fail({
+          duration: 2000,
+          message: `No tank found matching '${name}'`,
+        });
+        return;
+      } else {
+        let bestMatch = {
+          score: 0,
+          data: [],
+        };
+
+        // return a list, case non-sensitive
+        for (let i = 0; i < allTankData.length; i++) {
+          const tempScore = getMatchScore(name.toLowerCase(), allTankData[i].name.toLowerCase());
+          if (bestMatch.score < tempScore) {
+            bestMatch = {
+              score: tempScore,
+              data: [allTankData[i]],
+            };
+          } else if (bestMatch.score === tempScore) {
+            bestMatch = {
+              score: tempScore,
+              data: [...bestMatch.data, allTankData[i]],
+            };
+          }
+        }
+
+        if (bestMatch.data === null || bestMatch.data.name === null) {
+          Toast.fail({
+            duration: 2000,
+            message: `No tank found matching '${name}'`,
+          });
+        } else {
+          tankDataDisplay.value = bestMatch.data;
+          Toast.success({
+            duration: 2000,
+            message: `${bestMatch.data.length} matching results found`,
+          });
+        }
+      }
+    }
+
+    const onSearch = () => {
+      getTankDataByName(value.value);
+      value.value = "";
+    };
+
+    function getAllTankData() {
+      const url =
+        "https://api.worldoftanks.com/wot/encyclopedia/vehicles/?application_id=9ac9f425f534dca03b4ab84d40bb7310";
+      axios.get(url).then(
+        (response) => {
+          Object.values(response.data.data).forEach((tankData) => {
+            allTankData.push(tankData);
+          });
+          hasAllTankData = true;
+          Toast.success("Data fetched");
+        },
+        (error) => (this.content = error)
+      );
+    }
+
+    getAllTankData();
+
+    return {
+      tankDataDisplay,
+      allTankData,
+      getTankDataByTier,
+      getTankDataByName,
+      Button,
+      Search,
+      value,
+      onSearch,
+    };
+  },
+};
+</script>
+
 <style lang="scss">
+@import "./AppStyle.scss";
+
+body {
+  margin: 0px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
